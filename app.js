817e4bc0916122b056217c23c44cc747ea766b29
sanjo lawal <sanjolawal600@gmail.com>
2022-11-30 18:30:54 -0800

    commit

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
 const express = require(`express`);
 const app = express();
 
-<<<<<<< HEAD
 app.get(`/`, (req, res) => {
   console.log(`me`);
 });
-=======
-app.get('/', req , res) => {
-console.log(`me`}
-)
->>>>>>> 3fb59c07ae373e0c65f4e6e4ad244170dccd11d2
